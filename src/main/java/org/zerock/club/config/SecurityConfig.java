0dc2842193a3c2bf62073df8e0056f07fd936ba0
Kimhyunjin4455 <dp4455@naver.com>
2023-07-11 21:38:05 +0900

    p508

--- src/main/java/org/zerock/club/config/SecurityConfig.java
+++ src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -3,11 +3,14 @@ package org.zerock.club.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @Log4j2
@@ -27,5 +30,18 @@ public class SecurityConfig {
         log.info(user);
 
         return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests((auth)->{
+            auth.antMatchers("/sample/all").permitAll();
+            auth.antMatchers("/sample/member").hasRole("USER");
+        });
+        http.formLogin();
+        http.csrf().disable();
+        http.logout();
+
+        return http.build();
     }
 }
