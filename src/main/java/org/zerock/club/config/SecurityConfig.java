19f4afc6678b2b4e8db670ece4e3b04b61411f16
Kimhyunjin4455 <dp4455@naver.com>
2023-07-11 20:19:35 +0900

    p493

--- src/main/java/org/zerock/club/config/SecurityConfig.java
+++ src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -1,9 +1,31 @@
 package org.zerock.club.config;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @Log4j2
 public class SecurityConfig {
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(){
+        UserDetails user = User.builder()
+                .username("user1")
+                .password(passwordEncoder().encode("1111"))
+                .roles("USER").build();
+
+        log.info("userDetailsService............");
+        log.info(user);
+
+        return new InMemoryUserDetailsManager(user);
+    }
 }
