f7459a0c98b20a5e4cd00490342809b2bbc58625
Kimhyunjin4455 <dp4455@naver.com>
2023-07-17 17:31:52 +0900

    p543

--- src/main/java/org/zerock/club/security/service/ClubOAuth2UserDetailsService.java
+++ src/main/java/org/zerock/club/security/service/ClubOAuth2UserDetailsService.java
@@ -1,17 +1,33 @@
 package org.zerock.club.security.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.zerock.club.entity.ClubMember;
+import org.zerock.club.entity.ClubMemberRole;
+import org.zerock.club.repository.ClubMemberRepository;
+
+import java.util.Optional;
 
 @Log4j2
 @Service
+@RequiredArgsConstructor
 public class ClubOAuth2UserDetailsService extends DefaultOAuth2UserService {
+
+    private final ClubMemberRepository repository;
+
+    private final PasswordEncoder passwordEncoder;
+
+
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException{
+
         log.info("-------------------------");
         log.info("userRequest:" + userRequest);
 
@@ -27,6 +43,41 @@ public class ClubOAuth2UserDetailsService extends DefaultOAuth2UserService {
             log.info(k +" : "+v);
         });
 
+        String email = null;
+        if (clientName.equals("Google")){
+            email = oAuth2User.getAttribute("email");
+        }
+
+        log.info("EMAIL: " + email);
+
+        ClubMember member = saveSocialMember(email);
+
         return oAuth2User;
+
+
     }
+    private ClubMember saveSocialMember(String email){
+        // 기존에 동일한 이메일로 가입한 회원이 있는 경우에는 그대로 조회만
+        Optional<ClubMember> result = repository.findByEmail(email, true);
+
+        if (result.isPresent()){
+            return result.get();
+        }
+
+        // 없다면 회원 추가 패스워드는 1111, 이름은 이메일 주소
+        ClubMember clubMember = ClubMember.builder().email(email)
+                .name(email)
+                .password(passwordEncoder.encode("1111"))
+                .fromSocial(true)
+                .build();
+
+        clubMember.addMemberRole(ClubMemberRole.USER);
+
+        repository.save(clubMember);
+
+        return clubMember;
+
+    }
+
+
 }
